dbd1703b762cc80dddd0c4fe2ba137378687ca28
johan <johan.nordstrom.consulting@gmail.com>
2024-02-17 21:37:29 +0100

    Added more movies to database seed

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -30,6 +30,12 @@ async function seed() {
             { Title: "The Matrix Revolutions", Genre: "Action", PosterPath: "/lZpWprJqbIFpEV5M6Hd4z8zqCp3.jpg", BackdropPath: "/533xAMhhVyjTy8hwMUFEt5TuDfR.jpg" },
             { Title: "The Dark Knight Rises", Genre: "Action", PosterPath: "/vzvKcPQ4o7TjWeGIn0aGC9FeVNu.jpg", BackdropPath: "/bC6JDkO8oXReF9n2u8Z9Yf5ZSd7.jpg" },
             { Title: "The Godfather: Part II", Genre: "Drama", PosterPath: "/hek3koDUyRQk7FIhPXdeO9B8kPR.jpg", BackdropPath: "/poec6RqOKY9iSiIUmfyfPfiLtvB.jpg" },
+            { Title: "A Clockwork Orange", Genre: "Drama", PosterPath: "/4J8K3fU1Qz4v4jRzg4cghk5e0Q9.jpg", BackdropPath: "/mFq3ZwW8qS6F9U8bZGBU3Ml3Lmz.jpg" },
+            { Title: "2001: A Space Odyssey", Genre: "Science Fiction", PosterPath: "/90T7b2LIrL07ndYQBmSm09yqVEH.jpg", BackdropPath: "/pckdZ29bHj11hBsV3ZV5c3o5fUv.jpg" },
+            { Title: "Goodfellas", Genre: "Drama", PosterPath: "/pwpGfTImTGifEGgLb3s6LRPd4I6.jpg", BackdropPath: "/sw7mordbZxgITU877y0nZfF9U0b.jpg" },
+            { Title: "Blade Runner", Genre: "Science Fiction", PosterPath: "/p64TtbZGCElxQHpAMWmDHkWJlH2.jpg", BackdropPath: "/p64TtbZGCElxQHpAMWmDHkWJlH2.jpg" },
+            { TItle: "Blade Runner 2049", Genre: "Science Fiction", PosterPath: "/gajva2L0rPYkEWjzgFlBXCAVBE5.jpg", BackdropPath: "/8gq3wv4w9jLWv8bYKSLfV8F5l5l.jpg" },
+            { Title: "Fight Club", Genre: "Drama", PosterPath: "/adw6Lq9FiC9zjYEpOqfq03ituwp.jpg", BackdropPath: "/87hTDiay2N2qWyX4Ds7ybXi9h8I.jpg" },
         ];
 
         for (let mov of movs) {
