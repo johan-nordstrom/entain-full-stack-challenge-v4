64757785e92b7b9f5c036bc8dcfb76966be19419
johan <johan.nordstrom.consulting@gmail.com>
2024-02-18 19:52:58 +0100

    Add static module to serve react client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,27 @@
 import { Module } from '@nestjs/common';
 import { DrizzlePostgresModule } from '@knaadh/nestjs-drizzle-postgres';
 import { ConfigModule } from '@nestjs/config';
-import {DBConfigService} from "./database/dbconfig.service";
-import {MovieModule} from "./movie/movie.module";
+import { DBConfigService } from "./database/dbconfig.service";
+import { MovieModule } from "./movie/movie.module";
 import configuration from '../config/configuration';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'path';
 
 @Module({
   imports: [
-      MovieModule,
-      DrizzlePostgresModule.registerAsync({
-            tag: 'DB_DEV',
-            useClass: DBConfigService,
-      }),
-      ConfigModule.forRoot({
-          isGlobal: true,
-          load: [configuration]
-      },
-  )],
+    ServeStaticModule.forRoot({
+      rootPath: resolve('dist/client'),
+    }),
+    MovieModule,
+    DrizzlePostgresModule.registerAsync({
+      tag: 'DB_DEV',
+      useClass: DBConfigService,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration]
+    },
+    )],
 })
 
-export class AppModule {}
+export class AppModule { }
